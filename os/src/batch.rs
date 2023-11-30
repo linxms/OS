51822fbf308aaf7399c1079e7b0ffcd55ea25f2d
linx <2737573463@qq.com>
2023-11-30 23:23:53 +0800

    21301156完成了实验3
    完成了实验三，并重构了部分结构

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -1,10 +1,7 @@
 use core::arch::asm;
 use core::cell::RefCell;
 use lazy_static::*;
-use crate::trap::TrapContext;
 
-const USER_STACK_SIZE: usize = 4096 * 2;
-const KERNEL_STACK_SIZE: usize = 4096 * 2;
 const MAX_APP_NUM: usize = 16;
 const APP_BASE_ADDRESS: usize = 0x80400000;
 const APP_SIZE_LIMIT: usize = 0x20000;
@@ -20,44 +17,6 @@ struct AppManagerInner {
 }
 
 unsafe impl Sync for AppManager {}
-
-impl AppManagerInner {
-    pub fn print_app_info(&self) {
-        println!("[kernel] num_app = {}", self.num_app);
-        for i in 0..self.num_app {
-            println!("[kernel] app_{} [{:#x}, {:#x})", i, self.app_start[i], self.app_start[i + 1]);
-        }
-    }
-
-    unsafe fn load_app(&self, app_id: usize) {
-        if app_id >= self.num_app {
-            panic!("All applications completed!");
-        }
-
-        println!("[kernel] Loading app_{}", app_id);
-        // clear icache
-        asm!("fence.i");
-        // clear app area
-        (APP_BASE_ADDRESS..APP_BASE_ADDRESS + APP_SIZE_LIMIT).for_each(|addr| {
-            (addr as *mut u8).write_volatile(0);
-        });
-        let app_src = core::slice::from_raw_parts(
-            self.app_start[app_id] as *const u8,
-            self.app_start[app_id + 1] - self.app_start[app_id]
-        );
-        let app_dst = core::slice::from_raw_parts_mut(
-            APP_BASE_ADDRESS as *mut u8,
-            app_src.len()
-        );
-        app_dst.copy_from_slice(app_src);
-    }
-
-    pub fn get_current_app(&self) -> usize { self.current_app }
-
-    pub fn move_to_next_app(&mut self) {
-        self.current_app += 1;
-    }
-}
 
 lazy_static! {
     static ref APP_MANAGER: AppManager = AppManager {
@@ -77,6 +36,44 @@ lazy_static! {
             }
         }),
     };
+}
+
+impl AppManagerInner {
+    pub fn print_app_info(&self) {
+        println!("[kernel] num_app = {}", self.num_app);
+        for i in 0..self.num_app {
+            println!("[kernel] app_{} [{:#x}, {:#x})", i, self.app_start[i], self.app_start[i + 1]);
+        }
+    }
+
+    unsafe fn load_app(&self, app_id: usize) {
+        if app_id >= self.num_app {
+            panic!("All applications completed!");
+        }
+    
+        println!("[kernel] Loading app_{}", app_id);
+        // clear icache
+        asm!("fence.i");
+        // clear app area
+        (APP_BASE_ADDRESS..APP_BASE_ADDRESS + APP_SIZE_LIMIT).for_each(|addr| {
+            (addr as *mut u8).write_volatile(0);
+        });
+        let app_src = core::slice::from_raw_parts(
+            self.app_start[app_id] as *const u8,
+            self.app_start[app_id + 1] - self.app_start[app_id]
+        );
+        let app_dst = core::slice::from_raw_parts_mut(
+            APP_BASE_ADDRESS as *mut u8,
+            app_src.len()
+        );
+        app_dst.copy_from_slice(app_src);
+    }
+    
+    pub fn get_current_app(&self) -> usize { self.current_app }
+    
+    pub fn move_to_next_app(&mut self) {
+        self.current_app += 1;
+    }
 }
 
 pub fn init() {
@@ -103,7 +100,10 @@ pub fn run_next_app() -> ! {
     panic!("Unreachable in batch::run_current_app!");
 }
 
+use crate::trap::TrapContext;
 
+const USER_STACK_SIZE: usize = 4096 * 2;
+const KERNEL_STACK_SIZE: usize = 4096 * 2;
 
 #[repr(align(4096))]
 struct KernelStack {
@@ -118,6 +118,12 @@ struct UserStack {
 static KERNEL_STACK: KernelStack = KernelStack { data: [0; KERNEL_STACK_SIZE] };
 static USER_STACK: UserStack = UserStack { data: [0; USER_STACK_SIZE] };
 
+impl UserStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + USER_STACK_SIZE
+    }
+}
+
 impl KernelStack {
     fn get_sp(&self) -> usize {
         self.data.as_ptr() as usize + KERNEL_STACK_SIZE
@@ -126,11 +132,5 @@ impl KernelStack {
         let cx_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
         unsafe { *cx_ptr = cx; }
         unsafe { cx_ptr.as_mut().unwrap() }
-    }
-}
-
-impl UserStack {
-    fn get_sp(&self) -> usize {
-        self.data.as_ptr() as usize + USER_STACK_SIZE
     }
 }
